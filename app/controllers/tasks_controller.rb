4cf5104e41014fd0d460ea1bce89c16ee35e0dbf
Kaku <kaku.takanori.1026@gmail.com>
2019-07-27 16:07:48 +0000

    task_show 途中

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,6 +8,10 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
   
+  def show
+    @task = Task.find(params[:id])
+  end
+  
   def edit
     @task = Task.find(params[:id])
   end
