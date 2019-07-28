2686fc6948ca6c44f198425e4f66b924d902dcff
Kaku <kaku.takanori.1026@gmail.com>
2019-07-28 06:16:44 +0000

    ルーティング変更

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,10 +12,30 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
   
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:success] = "新規作成に成功しました。"
+      redirect_to task_url user
+    else
+      render 'new'
+    end
+  end
+  
   def edit
     @task = Task.find(params[:id])
   end
   
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    flash[:success] ="削除しました。"
+    redirect_to tasks_url
   end
+  
+  private
+    
+    def task_params
+      params.require(:task).permit(:task_name, :details, :user_id)
+    end
 end
